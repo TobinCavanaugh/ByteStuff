105b315c8bfba0bb8cae0dae2e05eee34ed4f3d5
Tobin Cavanaugh <tobincavanaugh@gmail.com>
2024-05-13 14:03:54 -0700

    screenshots

--- script.js
+++ script.js
@@ -18,12 +18,11 @@ function bitchange() {
     byteInput.value = Math.floor(bits / 8);
 
 
-    document.getElementById("bit_output").innerText =
-        `Min Unsigned: 0
-        Max Unsigned: ${BigInt(2 ** bits - 1).toString()}
+    document.getElementById("bit_output").innerText = `Min Unsigned: 0
+        Max Unsigned: ${BigInt(2n ** BigInt(bits) - 1n).toString()}
         ---
-        Min Signed: ${BigInt(-(2 ** (bits - 1))).toString()}
-        Max Signed: ${BigInt(2 ** (bits - 1) - 1).toString()}`;
+        Min Signed: ${BigInt(-(2n ** (BigInt(bits) - 1n))).toString()}
+        Max Signed: ${BigInt(2n ** (BigInt(bits) - 1n) - 1n).toString()}`;
 }
 
 function bytechange() {
@@ -59,32 +58,33 @@ function uintenter() {
 function numberscale() {
     const input = document.getElementById("numberscale_input");
     const values = {
-        0: "the known number of languages better than C",
-        1: "the amount of people who made this site",
-        8: "the average RAM size in GB of a home PC",
-        256: "the max value of a component of an 8 bit RGB value",
-        4528: "the number of transistors on a MOS Technology 6502 microcontroller",
-        2082240: "the number of pixels on a 1920x1080 display (1080p)",
-        3686400: "the number of pixels on a 2560x1440 display (1440p)",
-        8294400: "the number of pixels on a 3840x2160 display (4k)",
-        983571056: "the speed of light in feet per second",
-        4800000000: "the number of transistors on an Xbox One's GPU",
-        8109166279: "the number of people alive as of 5:24 on 2024/05/12",
-        67000000000: "the number of transistors on an M2 MAX CPU",
-        1099511627776: "one terabyte (TB) of data in bytes",
-        30000000000000: "the number of cells in the human body",
-        26184000000000000000000: "the size of the internet as of 2024/05/12 in bytes",
-        1000000000000000000000000: "the estimated number of stars in the universe",
-        880000000000000000000000000: "the diameter of the universe in meters",
-        3280000000000000000000000000000000000000000000000000000000000000000000000000000: "the number of particles in the known universe",
+        0n: "the known number of languages better than C",
+        1n: "the amount of people who made this site",
+        8n: "the average RAM size in GB of a home PC",
+        256n: "the max value of a component of an 8 bit RGB value",
+        4528n: "the number of transistors on a MOS Technology 6502 microcontroller",
+        2082240n: "the number of pixels on a 1920x1080 display (1080p)",
+        3686400n: "the number of pixels on a 2560x1440 display (1440p)",
+        8294400n: "the number of pixels on a 3840x2160 display (4k)",
+        983571056n: "the speed of light in feet per second",
+        4800000000n: "the number of transistors on an Xbox One's GPU",
+        8109166279n: "the number of people alive as of 5:24 on 2024/05/12",
+        67000000000n: "the number of transistors on an M2 MAX CPU",
+        1099511627776n: "one terabyte (TB) of data in bytes",
+        30000000000000n: "the number of cells in the human body",
+        26184000000000000000000n: "the size of the internet as of 2024/05/12 in bytes",
+        1000000000000000000000000n: "the estimated number of stars in the universe",
+        880000000000000000000000000n: "the diameter of the universe in meters",
+        3280000000000000000000000000000000000000000000000000000000000000000000000000000n: "the number of particles in the known universe",
     }
 
-    let lastKey = Number(Object.keys(values).pop());
+    let lastKey = BigInt(Object.keys(values).pop());
 
     // const closest = values.keys()
     for (let key in values) {
-        const a = Math.abs(input.value - key);
-        const b = Math.abs(input.value - lastKey);
+        const abs = (n) => (n < 0n) ? -n : n;
+        const a = abs(BigInt(input.value) - BigInt(key));
+        const b = abs(BigInt(input.value) - BigInt(lastKey));
 
         if (a < b) {
             lastKey = key;
@@ -117,7 +117,7 @@ function numberConvert() {
     const sciInput = document.getElementById("sci_input");
 
     eInput.value = Number(numInput.value).toExponential().toString();
-    sciInput.value = eformToSciform(eInput.value);
+    sciInput.value = eformToSciform(eInput.value.toString());
 }
 
 
@@ -141,8 +141,8 @@ function sciConvert() {
     const numInput = document.getElementById("num_input");
     const sciInput = document.getElementById("sci_input");
 
-    console.log(sciformToNum(sciInput.value));
-    numInput.value = sciformToNum(sciInput).toString();
+    console.log(":)");
+    numInput.value = sciformToNum(sciInput.value.toString()).toString();
 
     numberConvert();
 }
